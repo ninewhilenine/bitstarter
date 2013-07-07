35d130ea138e8e9f8f1a0fdae024c6f5c7e73fce
Thomas Repantis <thomas@ninewhilenine.org>
2013-07-07 11:40:09 -0400

    Read server response from an index.html file.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var infile = "index.html";
+var filecontents = fs.readFileSync(infile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(filecontents.toString());
 });
 
 var port = process.env.PORT || 5000;
